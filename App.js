6acdd90775167d04afcc66a9284116fa68d2923f
mahakantil <mahakantil95@gmail.com>
2026-08-05 19:42:18 +0530

    App.js

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ const path = require("path");
 const hbs = require("hbs");
 const cors = require('cors');
 const https = require('https');
-const Register = require("./Models/Register");
- const conn = require("./db/conn");
+const Register = require("./models/Register");
+const conn = require("./db/conn");
 const app = express();
 const port = process.env.PORT || 2020;
 
